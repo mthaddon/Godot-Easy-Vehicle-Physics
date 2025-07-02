74852da63989da700758438c724668d3189d0cb3
Tom Haddon <tom@haddons.net>
2025-07-02 11:37:39 +0200

    Move new script to script dir

--- addons/gevp/scripts/demo_arcade.gd
+++ addons/gevp/scripts/demo_arcade.gd
@@ -1,15 +1,9 @@
 extends Node3D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Setting the posisiton to the value below seems to work as expected.
 	# $VehicleController/VehicleRigidBody.teleport_position = Vector3(10, 1, 10)
 	# But setting it to this, we end up with the vehicle being moved to that
 	# position, and then something moves it "back"?
 	$VehicleController/VehicleRigidBody.teleport_position = Vector3(50, 1, 10)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
